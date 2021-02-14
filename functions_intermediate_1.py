2a75052c87e075f19ce86fe98c54dbd7ff9b7008
Eric H. Smith <22087301+bender-meemoo@users.noreply.github.com>
2021-02-13 22:54:16 -0500

    Add files via upload

---
+++ functions_intermediate_1.py
@@ -0,0 +1,16 @@
+import random
+def randInt(min = 0, max = 0):
+    num = min + max * random.random()
+
+    return int(num)
+
+#print(randInt(0,100)) 		    # should print a random integer between 0 to 100
+print(randInt(max=50)) 	    # should print a random integer between 0 to 50
+#print(randInt(min=50)) 	    # should print a random integer between 50 to 100
+#print(randInt(min=50, max=500))    # should print a random integer between 50 and 500
+# Here are a couple of important notes about using random.random() and rounding. (Create this function without using random.randInt() -- we are trying to build that method ourselves for this assignment!)
+
+# random.random() returns a random floating number between 0.000 and 1.000
+# random.random() * 50 returns a random floating number between 0.000 and 50.000
+# random.random() * 25 + 10 returns a random floating number between 10.000 and 35.000
+# round(num) returns the rounded integer value of num
